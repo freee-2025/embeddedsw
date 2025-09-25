1492764fc0b33f8f6b15c8b17d114e3cb5fdb8b2
Nobuyoshi Furihata <n_furihata@otari.co.jp>
2025-09-25 13:13:59 +0900

    change GEM3 to GEM0

--- lib/sw_apps/img_rcvry/misc/tools/xtopology_g.c
+++ lib/sw_apps/img_rcvry/misc/tools/xtopology_g.c
@@ -8,12 +8,14 @@
 
 struct xtopology_t xtopology[] = {
 	{
-		0xFF0E0000,
+//		0xFF0E0000,
+		0xFF0B0000,
 		xemac_type_emacps,
 		0x0,
 		0x0,
 		0xF8F00100,
-		XPAR_XEMACPS_3_INTR,
+//		XPAR_XEMACPS_3_INTR,
+		XPAR_XEMACPS_0_INTR,
 	},
 	{
 		0xFF0C0000,
